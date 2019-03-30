9d8f6fd38864ccfada0e3be2ad8e15b6c5c1ec66
Simon <zhengxiangpx@gmail.com>
2019-03-30 17:00:47 +0800

    提交代码

---
+++ src/main/java/com/learn/jcip/examples/DeadLockDemo.java
@@ -0,0 +1,40 @@
+package com.learn.jcip.examples;
+
+public class DeadLockDemo {
+    private static String A = "A";
+    private static String B = "B";
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+
+    private void deadLock() {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (A) {
+                    try {
+                        Thread.currentThread().sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (B) {
+                        System.out.println("1");
+                    }
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (B) {
+                    synchronized (A) {
+                        System.out.println("2");
+                    }
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
+}
