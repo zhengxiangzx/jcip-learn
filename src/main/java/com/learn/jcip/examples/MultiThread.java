5f54837d1bfa0bc1833add8ed54e9662ed1a7b0f
zhengxiangpx <zhengxiangpx@gmail.com>
2019-04-02 23:01:22 +0800

    添加

---
+++ src/main/java/com/learn/jcip/examples/MultiThread.java
@@ -0,0 +1,21 @@
+package com.learn.jcip.examples;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+/*
+ * 第四章Java并发编程基础
+ * */
+public class MultiThread {
+    public static void main(String[] args) {
+        //获取Java线程管理MXBean
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        //不需要获取同步的monitor和synchronizer信息，仅获取线程和线程栈信息
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false, false);
+        //遍历线程信息，仅打印线程ID和线程名称信息
+        for (ThreadInfo threadInfo : threadInfos) {
+            System.out.println("[" + threadInfo.getThreadId() + "]" + threadInfo.getThreadName());
+        }
+    }
+}
