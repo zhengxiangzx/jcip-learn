209bffb99b778c2a8c47b1e094c31821da796617
Simon <zhengxiangpx@gmail.com>
2019-04-09 09:23:53 +0800

    添加线程状态代码

---
+++ src/main/java/com/learn/jcip/examples/ThreadState.java
@@ -0,0 +1,50 @@
+package com.learn.jcip.examples;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        new Thread(new TimeWaiting(), "TimeWaitingThread").start();
+        new Thread(new Waiting(), "WaitingThread").start();
+        new Thread(new Blocked(), "BlockedThread-1").start();
+        new Thread(new Blocked(), "BlockedThread-2").start();
+    }
+
+    //该线程不断睡眠
+    static class TimeWaiting implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                SleepUtils.second(100);
+            }
+        }
+    }
+
+    //改线程在waiting.class实例上等待
+    static class Waiting implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (Waiting.class) {
+                    try {
+                        Waiting.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    //该线程在Blocked.class实例上加锁后，不会释放该锁
+    static class Blocked implements Runnable {
+
+        @Override
+        public void run() {
+            synchronized (Blocked.class) {
+                while (true) {
+                    SleepUtils.second(100);
+                }
+            }
+        }
+    }
+}
